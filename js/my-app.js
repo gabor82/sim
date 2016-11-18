e385628e8874bc371281962c8bc1fb31e1a29e25
speak82 <speak@speak.hu>
2016-11-18 11:03:32 +0100

    new

--- js/my-app.js
+++ js/my-app.js
@@ -14,23 +14,8 @@ var mainView = myApp.addView('.view-main', {
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
-
-
-cordova.plugins.notification.local.schedule({
-    id: 1,
-    title: "Production Jour fixe",
-    text: "Duration 1h",
-    firstAt: monday_9_am,
-    every: "week",
-    sound: "file://sounds/reminder.mp3",
-    icon: "http://icons.com/?cal_id=1",
-    data: { meetingId:"123#fg8" }
-});
- 
-cordova.plugins.notification.local.on("click", function (notification) {
-    joinMeeting(notification.data.meetingId);
-});
-
+    
+window.plugin.notification.local.add({ message: 'Great app!' });
  
 });
 
