d3e48c32522e883c78c8fd2356aa66434bab9969
speak82 <speak@speak.hu>
2016-11-18 10:01:46 +0100

    new

--- js/my-app.js
+++ js/my-app.js
@@ -14,6 +14,21 @@ var mainView = myApp.addView('.view-main', {
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
+
+ cordova.plugins.backgroundMode.setDefaults({ text:'Doing heavy tasks.'});
+    // Enable background mode 
+    cordova.plugins.backgroundMode.enable();
+ 
+    // Called when background mode has been activated 
+    cordova.plugins.backgroundMode.onactivate = function () {
+        setTimeout(function () {
+            // Modify the currently displayed notification 
+            cordova.plugins.backgroundMode.configure({
+                text:'Running in background for more than 5s now.'
+            });
+        }, 5000);
+    }
+
     checkConnection();
 });
 
